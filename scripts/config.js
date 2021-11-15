8ccf4b7e433d7c844c51792a107557df7fc813a3
Niek Mollers <info@sofane.nl>
2021-11-16 00:09:01 +0100

    312-Validating user input with JavaScript

--- scripts/config.js
+++ scripts/config.js
@@ -6,10 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayerName = formData.get('playername');
+  const enteredPlayerName = formData.get('playername').trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
 }

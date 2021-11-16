1542dfd78c044f78e6492df8da3709df3aa5e27b
Niek Mollers <info@sofane.nl>
2021-11-16 21:22:24 +0100

    318 End the game & add restart logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,30 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = 'none';
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (!players[0].name || !players[1].name) {
     alert('Please set custom player name for both players!');
     return;
   }
+
+  resetGameStatus();
 
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = 'block';
@@ -19,7 +41,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-  if (event.target.tagName !== 'LI') {
+  if (event.target.tagName !== 'LI' || gameIsOver) {
     return;
   }
 
@@ -38,7 +60,10 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
   switchPlayer();
@@ -87,4 +112,17 @@ function checkForGameOver() {
     return -1;
   }
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
